f4e2d0048ffcf699ac5d6a01e19d517ec6dc1be4
Tyler Cairney <tylercairney99@gmail.com>
2024-10-14 22:30:49 -0700

    test13

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://b-b-maintenances-services.vercel.app/' }));
 app.use(express.json()); // Parse JSON request bodies
 
 // Create MySQL connection pool
